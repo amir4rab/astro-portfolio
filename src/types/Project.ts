6815eb0ce333f2648552b42836ba9e088397a760
Amir Arab <amir4rab@gmail.com>
2022-02-24 10:18:38 +0330

    added types

---
+++ src/types/Project.ts
@@ -0,0 +1,36 @@
+export type Project = {
+  title: string,
+  slug: string,
+  thumbnail: string | null,
+  specialProject: boolean,
+  github: string | null,
+  websiteAddress: string | null,
+  npmPackage: string | null,
+  tags: string[],
+  shortInfo: string,
+  astro: {
+    header: object[],
+    source: string,
+    html: string
+  },
+  content: {
+    header: string[],
+    source: string,
+    html: string
+  },
+  file: {
+    href: string,
+    origin: string,
+    protocol: string,
+    username: '',
+    password: string,
+    host: string,
+    hostname: string,
+    port: string
+    pathname: string,
+    search: string,
+    searchParams: {},
+    hash: string,
+  },
+  url: '/projects/web3-hot-wallet'
+}
