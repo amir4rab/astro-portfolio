89140d30a8a1c1be5535bf687eecb56c9499962f
Amir Arab <amir4rab@gmail.com>
2022-02-26 15:07:28 +0330

    add right sidebar

---
+++ src/components/rightSidebar/rightSidebar.svelte
@@ -0,0 +1,10 @@
+<script>
+  import './rightSidebar.scss';
+  export let contentArr;
+</script>
+
+<nav aria-label='inner-page-nav' class="rightSidebar">
+  {#each contentArr as content ( content.slug )}
+    <a href={`#${content.slug}`} class={`link depth-${content.depth}`}>{content.text}</a>
+  {/each}
+</nav>
